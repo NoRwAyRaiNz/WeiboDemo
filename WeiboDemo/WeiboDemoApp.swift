63c637bc708f1f34f09689e3eed1e297abe2cd60
JamieinYourArea <norwayrainz@gmail.com>
2021-03-05 19:53:47 +0800

    Initial Commit

---
+++ WeiboDemo/WeiboDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiboDemoApp.swift
+//  WeiboDemo
+//
+//  Created by Jamie on 2021/3/5.
+//
+
+import SwiftUI
+
+@main
+struct WeiboDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
